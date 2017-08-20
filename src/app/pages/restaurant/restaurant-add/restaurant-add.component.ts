4719d458a5a9a7902f9ca49d59d203493e19a420
TOxxTOxx <tovannapha2@gmail.com>
2017-08-20 23:35:21 +0900

    20170820 edit restaurant-add.html

--- src/app/pages/restaurant/restaurant-add/restaurant-add.component.ts
+++ src/app/pages/restaurant/restaurant-add/restaurant-add.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantAddComponent implements OnInit {
 
+  retaurant_name:any;
+  retaurant_type:any;
+  retaurant_phone:any;
+  retaurant_address:any;
+  retaurant_location_x:any;
+  retaurant_location_y:any;
+  retaurant_tables:any;
+  retaurant_seats:any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add_restaurant(){
+    console.log(this.retaurant_name);
+    console.log(this.retaurant_type);
+    console.log(this.retaurant_phone);
+    console.log(this.retaurant_address);
+    console.log(this.retaurant_address);
+    console.log(this.retaurant_location_x);
+    console.log(this.retaurant_location_y);
+    console.log(this.retaurant_tables);
   }
 
 }
